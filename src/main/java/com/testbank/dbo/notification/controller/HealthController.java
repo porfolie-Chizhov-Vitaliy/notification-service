6c184e9c53acd1331ef9510bb7a5919058456108
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-11-29 23:12:09 +0300

    update controller annotaion swagger

--- src/main/java/com/testbank/dbo/notification/controller/HealthController.java
+++ src/main/java/com/testbank/dbo/notification/controller/HealthController.java
@@ -2,19 +2,21 @@ package com.testbank.dbo.notification.controller;
 
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health Notification API", description = "Проверка запуска Java-приложения")
 public class HealthController {
-
+    @Operation(summary = "Проверка запуска Java-приложения")
+    @ApiResponse(responseCode = "200", description = "Сервис запущен")
+    @ApiResponse(responseCode = "400", description = "Сервис не запушен")
     @GetMapping("/health")
     public String health() {
-        return "Notification Service is UP and running!";
+        return "notification-service запущен";
     }
 
-    @GetMapping("/api/notifications/hello")
-    public String hello() {
-        return "Hello from Notification Service!";
-    }
 }
--- src/main/java/com/testbank/dbo/notification/controller/NotificationController.java
+++ src/main/java/com/testbank/dbo/notification/controller/NotificationController.java
@@ -2,17 +2,22 @@ package com.testbank.dbo.notification.controller;
 import com.testbank.dbo.notification.Repository.NotificationRepository;
 import com.testbank.dbo.notification.dto.*;
 import com.testbank.dbo.notification.entity.NotificationEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 @RestController
+@Tag(name = "Notification API", description = "Операции с уведомлениями")
 @RequestMapping("/api/notifications")
 public class NotificationController {
 
     @Autowired
     private NotificationRepository notificationRepository;
-
+    @Operation(summary = "Create notification", description = "Создание уведомление")
+    @ApiResponse(responseCode = "200", description = "Успешно создано уведомление")
     @PostMapping
     public NotificationResponse createNotification(@RequestBody NotificationRequest request) {
         NotificationEntity notification = new NotificationEntity();
@@ -23,21 +28,24 @@ public class NotificationController {
         NotificationEntity savedNotification = notificationRepository.save(notification);
         return convertToResponse(savedNotification);
     }
-
+    @Operation(summary = "Get All Notifications", description = "Показать все уведомления")
+    @ApiResponse(responseCode = "200", description = "Отображение всех уведомление")
     @GetMapping
     public List<NotificationResponse> getAllNotifications() {
         return notificationRepository.findAll().stream()
                 .map(this::convertToResponse)
                 .collect(Collectors.toList());
     }
-
+    @Operation(summary = "Get Notification where id", description = "Показать уведомление по id")
+    @ApiResponse(responseCode = "200", description = "Отображение уведомление по id")
     @GetMapping("/{id}")
     public NotificationResponse getNotification(@PathVariable Long id) {
         return notificationRepository.findById(id)
                 .map(this::convertToResponse)
                 .orElse(null);
     }
-
+    @Operation(summary = "Put Notification mark-sent", description = "Изменить статус на SENT")
+    @ApiResponse(responseCode = "200", description = "Изменено уведомление на статус SENT")
     // Метод для пометки уведомления как отправленного
     @PutMapping("/{id}/mark-sent")
     public NotificationResponse markAsSent(@PathVariable Long id) {
@@ -59,4 +67,10 @@ public class NotificationController {
         response.setSentAt(notification.getSentAt());
         return response;
     }
+
+    @GetMapping("/simple-test")
+    public String simpleTest() {
+        return "Simple test works! " ;
+    }
+
 }
